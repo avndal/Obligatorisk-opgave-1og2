e2804791f688a78fdee176c1e51380f148e299cd
Rasmus Avndal Hansen <rah001@edu.zealand.dk>
2024-09-12 11:28:59 +0200

    Add project files.

---
+++ ClassLibraryTrophy/Trophy.cs
@@ -0,0 +1,39 @@
+﻿public class Trophy
+{
+    public int Id { get; set; }
+    public string? Competition { get; set; }
+    public int Year { get; set; }
+
+
+    public override string ToString()
+    {
+        return $"Id = {Id} Competition = {Competition} Year = {Year}";
+    }
+
+    
+    public void ValidateCompetition()
+    {
+        if (Competition==null)
+        {
+            throw new ArgumentNullException("Competition cannnot be null");
+        }
+        if (Competition.Length <= 3)
+        {
+            throw new ArgumentOutOfRangeException("Competition length must be at least 3");
+        }
+
+
+    }
+
+    public void ValidateYear()
+    {
+        if (Year <= 1970 || Year >= 2024)
+        {
+            throw new ArgumentOutOfRangeException("Please select a number in range");
+        }
+
+
+    }
+
+}
+
---
+++ ClassLibraryTrophyTests1/TrophyTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class TrophyTests
+    {
+
+        private readonly Trophy _trophy = new Trophy() { Id = 1, Competition = "Football", Year = 1992 };
+        private readonly Trophy _nullTitle = new Trophy() { Id = 2, Year = 1992 };
+        private readonly Trophy _emptyTitle = new Trophy() { Id = 3, Competition = "", Year = 1992 };
+        private readonly Trophy _yearLow = new Trophy() { Id = 4, Competition = "Football", Year = 1892 };
+
+
+
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            //Arrange
+
+            //Act 
+
+            //Assert
+            Assert.AreEqual("1 Football 1992", _trophy.ToString());
+
+        }
+
+        [TestMethod()]
+        public void ValidateCompetitionTest()
+        {
+            //Arrange
+
+            //Act 
+
+            //Assert
+        }
+
+        [TestMethod()]
+        public void ValidateYearTest()
+        {
+            //Arrange
+
+            //Act 
+
+            //Assert
+        }
+
+    }
+}
